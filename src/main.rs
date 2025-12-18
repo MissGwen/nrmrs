ed97495fd8c6af86a0b758ce8bc328d6819a2499
李源栋 <1526566980@qq.com>
2025-12-18 14:15:23 +0800

    feat(cli): 引入 clap 实现命令行参数解析
    新增 `clap` 依赖以支持结构化的命令行接口，并实现 `ls` 子命令用于列出所有注册表。
    移除了原有的简单参数判断逻辑，为后续扩展更多子命令奠定基础。

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,68 @@
-use std::{env, error, result};
+use clap::{Parser, Subcommand};
+use std::{error, result};
 mod database;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // /// Name of the person to greet
+    // #[arg(short, long)]
+    // name: String,
+
+    // /// Number of times to greet
+    // #[arg(short, long, default_value_t = 1)]
+    // count: u8,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// List all the registries
+    Ls {},
+    // /// 删除指定ID的TODO任务
+    // Delete {
+    //     /// 任务ID（必选）
+    //     #[arg(short, long)]
+    //     id: u32,
+    // },
+}
 
 fn main() -> result::Result<(), Box<dyn error::Error>> {
     let connection = database::registry::DatabaseManager::init()?;
-    // 接受命令行参数
-    let args: Vec<String> = env::args().collect();
-    // 判断参数是否为ls
-    if args.len() > 1 && args[1] == "ls" {
-        let packages = connection.find_all()?;
-        for (name, url) in packages {
-            println!("* {:<15} {}", name, url);
-        }
+    // let args = Args::parse();
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Ls {} => {
+            let packages = connection.find_all()?;
+            for (name, url) in packages {
+                println!("* {:<15} {}", name, url);
+            }
+        } // Commands::Delete { id } => {
+          //     println!("删除任务ID：{}", id);
+          // }
     }
-    // connection.create_registry("taobao", "https://registry.npmmirror.com/")?;
+    // for _ in 0..args.count {
+    //     println!("Hello {}!", args.name);
+    // }
+
     Ok(())
+
+    // // 接受命令行参数
+    // let args: Vec<String> = env::args().collect();
+    // // 判断参数是否为ls
+    // if args.len() > 1 && args[1] == "ls" {
+    //     let packages = connection.find_all()?;
+    //     for (name, url) in packages {
+    //         println!("* {:<15} {}", name, url);
+    //     }
+    // } else {
+    //     println!("npm registry manager! you can use ls to list all registries!")
+    // }
+    // Ok(())
 }
